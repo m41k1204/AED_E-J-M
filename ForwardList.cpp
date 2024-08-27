c014199ba6438f4cba22b30cd5eda1f13f570157
EduardoAragon11 <eduardo.aragon@utec.edu.pe>
2024-08-27 15:19:37 -0500

    ForwardList init

---
+++ ForwardList.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+struct Node{
+	int data;
+	Node* next;
+};
+
+class ForwardList{
+private:
+	Node* head;
+public:
+
+};
+
+int main(){
+	
+	return 0;
+}
