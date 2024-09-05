6b1855b2562e1ae7850bed26964e7f169688bee2
jorge-qs <jorge.quenta@utec.edu.pe>
2024-09-05 14:37:11 -0500

    Commitsito de la 4 y 6

---
+++ continuas/continua_3/p6.cpp
@@ -0,0 +1,48 @@
+/**
+* Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) return nullptr;
+
+        int interval = 1;
+        int n = lists.size();
+
+        while (interval < n) {
+            for (int i = 0; i < n - interval; i += interval * 2) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+            }
+            interval *= 2;
+        }
+
+        return lists[0];
+    }
+
+private:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+
+        while (l1 != nullptr && l2 != nullptr) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+
+        tail->next = l1 != nullptr ? l1 : l2;
+        return dummy->next;
+    }
+};
