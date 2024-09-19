8dafde99aa1cd90be39e787286c03645e1768bbd
jorge-qs <165625725+jorge-qs@users.noreply.github.com>
2024-09-19 14:38:54 -0500

    Update p2.cpp

--- continuas/continua_5/p2.cpp
+++ continuas/continua_5/p2.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int ans = 0;
+        vector<int> freq(254,0);
+        int l = 0;
+        int r = 0;
 
+        for (auto c:s){
+            freq[(int) c] ++;
+            while (freq[(int)c]>1){
+                freq[(int) s[l]] --;
+                l++;
+            }
+            ans = max (ans, r - l + 1);
+            r++;
+        }
+        return ans;
+    }
+};
