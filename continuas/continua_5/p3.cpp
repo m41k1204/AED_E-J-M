d1e700acb59776086e0387492db62cb2b0bdcdad
EduardoAragon11 <166084429+EduardoAragon11@users.noreply.github.com>
2024-09-19 14:49:17 -0500

    Update p3.cpp

--- continuas/continua_5/p3.cpp
+++ continuas/continua_5/p3.cpp
@@ -1,1 +1,29 @@
-
+class Solution {
+public:
+    bool eq (int li, int lj, int i, int j){
+        if(li == i && lj == j) return false;
+        if(lj == j) return true;
+        if(li == i) return true;
+        if(li/3 == i/3 && lj/3 == j/3) return true;
+        return false;
+    }
+    bool check(vector<vector<char>>& board, int li, int lj){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(eq(li, lj, i, j)){
+                    if(board[li][lj]==board[i][j]) return true;
+                }
+            }
+        }
+        return false;
+    }
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j] == '.') continue;
+                if(check(board, i, j)) return false;
+            }
+        }
+        return true;
+    }
+};
