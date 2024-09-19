3df9a79b348ea4ac5ba4443849cc3dd45bba0e8a
EduardoAragon11 <166084429+EduardoAragon11@users.noreply.github.com>
2024-09-19 14:34:34 -0500

    Update p1.cpp

--- continuas/continua_5/p1.cpp
+++ continuas/continua_5/p1.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    map<char, int> mp = {{'I', 1},{'V', 5},{'X', 10},{'L', 50},{'C', 100},{'D', 500}, {'M', 1000}};
+    int romanToInt(string s) {
+        s+="I";
+        int res = 0;
+        for(int i = 0; i<s.size()-1;i++){
+            if(mp[s[i]] < mp[s[i+1]]) res-= mp[s[i]];
+            else res+= mp[s[i]];
+        }
+        return res;
+    }
+};
