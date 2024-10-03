80c98f542a06a4a44fb76bf1f832c584f6d8daec
EduardoAragon11 <166084429+EduardoAragon11@users.noreply.github.com>
2024-10-03 14:42:03 -0500

    Update p2.cpp

--- continuas/continua_7/p2.cpp
+++ continuas/continua_7/p2.cpp
@@ -1,1 +1,29 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root == nullptr) return root;
+        if(root->val < key) root->right = deleteNode(root->right, key);
+        else if(root->val > key) root->left = deleteNode(root->left, key);
+        else {
+            if(root->right == nullptr) return root->left;
+            if(root->left == nullptr) return root->right;
+            TreeNode* ptr = root->right;
+            while(ptr->left != nullptr) ptr = ptr->left;
+            ptr->left = root->left->right;
+            root->left->right = root->right;
+            return root->left;
+        }
+        return root;
+    }
+};
