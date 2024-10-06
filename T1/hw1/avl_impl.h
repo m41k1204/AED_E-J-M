9fa620dc338e7c943ccabd352117151925cf989e
jorge-qs <165625725+jorge-qs@users.noreply.github.com>
2024-10-05 23:21:08 -0500

    Add files via upload

---
+++ T1/hw1/avl_impl.h
@@ -0,0 +1,276 @@
+#ifndef AVL_IMPL_H
+#define AVL_IMPL_H
+
+#include <algorithm>
+#include <cstdlib>
+#include <vector>
+
+#include "avl.h"
+
+// AVLNode
+
+template <typename T>
+AVLNode<T>::AVLNode(T value)
+        : data(value), left(nullptr), right(nullptr), height(0) {}
+
+// AVLTree
+
+template <typename T>
+AVLTree<T>::AVLTree() : root(nullptr) {}
+
+template <typename T>
+int AVLTree<T>::height(AVLNode<T>* node) {
+    if (node == nullptr)
+        return -1;
+    return node->height;
+}
+
+template <typename T>
+int AVLTree<T>::getBalance(AVLNode<T>* node) {
+    if (node == nullptr) {
+        return 0;
+    }
+    return height(node->left) - height(node->right);
+}
+
+template <typename T>
+AVLNode<T>* AVLTree<T>::rightRotate(AVLNode<T>* y) {
+    AVLNode<T>* x = y->left;
+    AVLNode<T>* T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = std::max(height(y->left), height(y->right)) + 1;
+    x->height = std::max(height(x->left), height(x->right)) + 1;
+
+    return x;
+}
+
+template <typename T>
+AVLNode<T>* AVLTree<T>::leftRotate(AVLNode<T>* x) {
+    AVLNode<T>* y = x->right;
+    AVLNode<T>* T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = std::max(height(x->left), height(x->right)) + 1;
+    y->height = std::max(height(y->left), height(y->right)) + 1;
+
+    return y;
+}
+
+template <typename T>
+AVLNode<T>* AVLTree<T>::minValueNode(AVLNode<T>* node) {
+    AVLNode<T>* current = node;
+
+    while (current->left != nullptr) {
+        current = current->left;
+    }
+
+    return current;
+}
+
+template <typename T>
+AVLNode<T>* AVLTree<T>::insert(AVLNode<T>* node, T key) {
+    if (node == nullptr)
+        return new AVLNode<T>(key);
+
+    if (key < node->data)
+        node->left = insert(node->left, key);
+    else if (key > node->data)
+        node->right = insert(node->right, key);
+    else
+        return node;
+
+    node->height = std::max(height(node->left), height(node->right)) + 1;
+
+    int balance = getBalance(node);
+
+    if (balance > 1 && key < node->left->data)
+        return rightRotate(node);
+
+    if (balance < -1 && key > node->right->data)
+        return leftRotate(node);
+
+    if (balance > 1 && key > node->left->data) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    if (balance < -1 && key < node->right->data) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
+}
+
+template <typename T>
+AVLNode<T>* AVLTree<T>::remove(AVLNode<T>* root, T key) {
+    if (root == nullptr)
+        return root;
+
+    if (key < root->data)
+        root->left = remove(root->left, key);
+    else if (key > root->data)
+        root->right = remove(root->right, key);
+    else {
+        if ((root->left == nullptr) || (root->right == nullptr)) {
+            AVLNode<T>* temp = root->left ? root->left : root->right;
+
+            if (temp == nullptr) {
+                temp = root;
+                root = nullptr;
+            }
+            else
+                *root = *temp;
+
+            delete temp;
+        }
+        else {
+            AVLNode<T>* temp = minValueNode(root->right);
+
+            root->data = temp->data;
+            root->right = remove(root->right, temp->data);
+        }
+    }
+
+    if (root == nullptr)
+        return root;
+
+    root->height = std::max(height(root->left), height(root->right)) + 1;
+
+    int balance = getBalance(root);
+
+    if (balance > 1 && getBalance(root->left) >= 0)
+        return rightRotate(root);
+
+    if (balance > 1 && getBalance(root->left) < 0) {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+
+    if (balance < -1 && getBalance(root->right) <= 0)
+        return leftRotate(root);
+
+    if (balance < -1 && getBalance(root->right) > 0) {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+
+    return root;
+}
+
+// Búsqueda
+template <typename T>
+bool AVLTree<T>::search(AVLNode<T>* node, T key) {
+    if (node == nullptr)
+        return false;
+    if (key == node->data)
+        return true;
+    else if (key < node->data)
+        return search(node->left, key);
+    else
+        return search(node->right, key);
+}
+
+// Recorridos
+template <typename T>
+void AVLTree<T>::preorder(AVLNode<T>* node, std::vector<T>& ret) {
+    if (node == nullptr)
+        return;
+    ret.push_back(node->data);
+    preorder(node->left, ret);
+    preorder(node->right, ret);
+}
+
+template <typename T>
+void AVLTree<T>::inorder(AVLNode<T>* node, std::vector<T>& ret) {
+    if (node == nullptr)
+        return;
+    inorder(node->left, ret);
+    ret.push_back(node->data);
+    inorder(node->right, ret);
+}
+
+template <typename T>
+void AVLTree<T>::postorder(AVLNode<T>* node, std::vector<T>& ret) {
+    if (node == nullptr)
+        return;
+    postorder(node->left, ret);
+    postorder(node->right, ret);
+    ret.push_back(node->data);
+}
+
+template <typename T>
+int AVLTree<T>::computeHeight(AVLNode<T>* node) {
+    if (node == nullptr)
+        return -1;
+    int leftHeight = computeHeight(node->left);
+    int rightHeight = computeHeight(node->right);
+    return std::max(leftHeight, rightHeight) + 1;
+}
+
+/// Funciones Públicas
+
+template <typename T>
+void AVLTree<T>::insert(T key) {
+    root = insert(root, key);
+}
+
+template <typename T>
+void AVLTree<T>::remove(T key) {
+    root = remove(root, key);
+}
+
+template <typename T>
+bool AVLTree<T>::search(T key) {
+    return search(root, key);
+}
+
+template <typename T>
+std::vector<T> AVLTree<T>::preorderTraversal() {
+    std::vector<T> ret;
+    preorder(root, ret);
+    return ret;
+}
+
+template <typename T>
+std::vector<T> AVLTree<T>::inorderTraversal() {
+    std::vector<T> ret;
+    inorder(root, ret);
+    return ret;
+}
+
+template <typename T>
+std::vector<T> AVLTree<T>::postorderTraversal() {
+    std::vector<T> ret;
+    postorder(root, ret);
+    return ret;
+}
+
+template <typename T>
+int AVLTree<T>::height() {
+    return computeHeight(root);
+}
+
+template <typename T>
+bool AVLTree<T>::isBalanced(AVLNode<T>* node) {
+    if (node == nullptr)
+        return true;
+
+    int balance = getBalance(node);
+    if (balance > 1 || balance < -1)
+        return false;
+
+    return isBalanced(node->left) && isBalanced(node->right);
+}
+
+template <typename T>
+bool AVLTree<T>::isBalanced() {
+    return isBalanced(root);
+}
+
+#endif
