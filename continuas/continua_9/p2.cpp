8da846d2d74f6d2c2c7071f4817214fbd58e937d
m41k1204 <155772773+m41k1204@users.noreply.github.com>
2024-10-24 14:49:09 -0500

    Update p2.cpp

--- continuas/continua_9/p2.cpp
+++ continuas/continua_9/p2.cpp
@@ -1,1 +1,72 @@
 
+struct TrieNode {
+    std::array<TrieNode*, 26> children;
+    TrieNode* parent;
+    bool isFinal = false;
+    TrieNode() : parent(nullptr) {
+        children.fill(nullptr); 
+    }
+    void addNode(TrieNode* node, int index) {
+        children[index] = node;
+    }
+};
+
+class Trie {
+
+public:
+    TrieNode* root;
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        insertHelper(word, root);
+    }
+    
+    void insertHelper(string &word, TrieNode* node) {
+        if (word.size() == 0) {
+            node->isFinal = true;
+            return;
+        }
+
+        int index = word[0] - 'a';
+        if (node->children[index] == nullptr) {
+            node->children[index] = new TrieNode();
+        }
+
+        insertHelper(word.erase(0,1), node->children[index]);
+    }
+    
+    bool search(string word) {
+        return searchHelper(word, root);
+    }
+
+    bool searchHelper(string &word, TrieNode* node) {
+        if (word.size() == 0) {
+            return node->isFinal;
+        }
+
+        int index = word[0] - 'a';
+        if (node->children[index] == nullptr) {
+            return false;
+        }
+
+        return searchHelper(word.erase(0,1), node->children[index]);
+    }
+    
+    bool startsWith(string prefix) {
+        return startWithHelper(prefix, root);
+    }
+
+    bool startWithHelper(string &prefix, TrieNode* node) {
+        if (prefix.size() == 0) return true;
+        
+
+        int index = prefix[0] - 'a';
+        if (node->children[index] == nullptr) {
+            return false;
+        }
+
+        return startWithHelper(prefix.erase(0, 1), node->children[index]);
+    }
+};
