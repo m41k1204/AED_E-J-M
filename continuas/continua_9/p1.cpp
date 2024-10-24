fbd4aca823f89a015c22d8105f59c5050d008a0d
m41k1204 <155772773+m41k1204@users.noreply.github.com>
2024-10-24 14:48:28 -0500

    Update p1.cpp

--- continuas/continua_9/p1.cpp
+++ continuas/continua_9/p1.cpp
@@ -1,1 +1,55 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string output = "";
+        TrieNode* root = new TrieNode();
+        for (auto s: strs) {
+            if (s == "") return output;
+            insert(root, s);
+        }
+        while (true) {
+            char letter = getChild(root);
+            if (letter == '$') return output;
+            output.push_back(letter);
+            int index = letter - 'a';
+            root = root -> children[index];
+        }
+        return output;
+
+
+    }
+
+    struct TrieNode {
+        std::array<TrieNode*, 26> children;
+        bool isFinal = false;
+        TrieNode() {
+            children.fill(nullptr);
+        }
+    };
+    void insert(TrieNode* root, string &word) {
+        for (auto c: word) {
+            int index = c - 'a';
+            if (root -> children[index] == nullptr) {
+                root -> children[index] = new TrieNode();
+            }
+            root = root -> children[index];
+        }
+        root -> isFinal = true;
+    }
+
+    char getChild(TrieNode* root) {
+        char letter = '$';
+        int amount = 0;
+        for (int i = 0; i < 26; i++) {
+            if (root -> children[i] != nullptr && !root -> isFinal) {
+                letter = 'a' + i;
+                amount++;
+            }
+        }
+        if (amount == 1) return letter;
+        return '$';
+    }
+
+};
+
 
