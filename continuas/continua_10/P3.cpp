bce82d6a8e4d4f62e7d37af40cac5b6270cda8f5
jorge-qs <165625725+jorge-qs@users.noreply.github.com>
2024-10-31 14:46:22 -0500

    Actualizar P3.cpp

--- continuas/continua_10/P3.cpp
+++ continuas/continua_10/P3.cpp
@@ -1,1 +1,32 @@
+class Solution {
+public:
+    bool isBipartite(std::vector<std::vector<int>>& graph) {
+        int n = graph.size();
+        std::vector<int> color(n, -1);  
 
+        for (int i = 0; i < n; ++i) {
+            if (color[i] == -1) {
+                if (!dfs(i, 0, color, graph)) {
+                    return false;
+                }
+            }
+        }
+
+        return true; 
+    }
+
+private:
+    bool dfs(int node, int c, std::vector<int>& color, const std::vector<std::vector<int>>& graph) {
+        color[node] = c;
+        for (int neighbor : graph[node]) {
+            if (color[neighbor] == -1) {
+                if (!dfs(neighbor, 1 - c, color, graph)) {
+                    return false;
+                }
+            } else if (color[neighbor] == c) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
