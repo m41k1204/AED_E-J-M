fcde436e5a021fddceb507b0e11fd57a0fa31b42
jorge-qs <165625725+jorge-qs@users.noreply.github.com>
2024-10-31 14:16:53 -0500

    Update P2.cpp

--- continuas/continua_10/P2.cpp
+++ continuas/continua_10/P2.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (node == nullptr) {
+            return nullptr;
+        }
 
+        std::unordered_map<Node*, Node*> clonedNodes;
+        return cloneNode(node, clonedNodes);
+    }
+
+private:
+    Node* cloneNode(Node* node, std::unordered_map<Node*, Node*>& clonedNodes) {
+        if (clonedNodes.find(node) != clonedNodes.end()) {
+            return clonedNodes[node];
+        }
+
+        Node* clone = new Node(node->val);
+        clonedNodes[node] = clone;
+
+        for (Node* neighbor : node->neighbors) {
+            clone->neighbors.push_back(cloneNode(neighbor, clonedNodes));
+        }
+
+        return clone;
+    }
+};
