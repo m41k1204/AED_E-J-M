6f830992a0fd750f6b961d06ac07528af37317fd
Jorge <jorge.quenta@utec.edu.pe>
2024-11-04 22:31:39 -0500

    hw3 terminado

--- homework/hw3/hw3.h
+++ homework/hw3/hw3.h
@@ -18,24 +18,36 @@ automático, con el mismo prototipo mostrado en este archivo.
 
 template <typename T>
 struct BST {
-    BST();
+private:
+    struct Node {
+        T value;
+        Node* left;
+        Node* right;
 
+        explicit Node(T val) : value(val), left(nullptr), right(nullptr) {}
+    };
+
+    Node* root;
+
+    void insert(T const& value, Node*& node);
+    bool contains(T const& value, Node* node) const;
+    Node* remove(T const& value, Node* node);
+    Node* findMin(Node* node) const;
+    Node* findMax(Node* node) const;
+    size_t size(Node* node) const;
+    void clear(Node*& node);
+
+public:
+    BST();
     ~BST();
 
     void insert(T const& value);
-
     bool search(T const& value) const;
-
     void remove(T const& value);
-
     size_t size() const;
-
     T findMin() const;
-
     T findMax() const;
-
     bool isEmpty() const;
-
     void clear();
 };
 
--- homework/hw3/hw3_impl.h
+++ homework/hw3/hw3_impl.h
@@ -4,59 +4,155 @@
 #include <algorithm>
 #include <cstdlib>
 #include <vector>
-
+#include <stdexcept>
 #include "hw3.h"
 
 template <typename T>
-BST<T>::BST() {
-  // COMPLETE HERE
-}
+BST<T>::BST() : root(nullptr) {}
 
 template <typename T>
 BST<T>::~BST() {
-  // COMPLETE HERE
+    clear();
 }
 
 template <typename T>
 void BST<T>::insert(T const& value) {
-  // COMPLETE HERE
+    insert(value, root);
 }
 
 template <typename T>
 bool BST<T>::search(T const& value) const {
-  // COMPLETE HERE
+    return contains(value, root);
 }
 
 template <typename T>
 void BST<T>::remove(T const& value) {
-  // COMPLETE HERE
+    root = remove(value, root);
 }
 
 template <typename T>
 size_t BST<T>::size() const {
-  // COMPLETE HERE
-  return 0;
+    return size(root);
 }
 
 template <typename T>
 T BST<T>::findMin() const {
-  // COMPLETE HERE
+    if (root == nullptr) {
+        throw std::runtime_error("BST is empty");
+    }
+    return findMin(root)->value;
 }
 
 template <typename T>
 T BST<T>::findMax() const {
-  // COMPLETE HERE
+    if (root == nullptr) {
+        throw std::runtime_error("BST is empty");
+    }
+    return findMax(root)->value;
 }
 
 template <typename T>
 bool BST<T>::isEmpty() const {
-  // COMPLETE HERE
+    return root == nullptr;
 }
 
 template <typename T>
 void BST<T>::clear() {
-  // COMPLETE HERE
+    clear(root);
 }
 
+// Private functions
+
+template <typename T>
+typename BST<T>::Node* BST<T>::findMin(Node* node) const {
+    if (node == nullptr) {
+        return nullptr;
+    }
+    while (node->left != nullptr) {
+        node = node->left;
+    }
+    return node;
+}
+
+template <typename T>
+typename BST<T>::Node* BST<T>::findMax(Node* node) const {
+    if (node == nullptr) {
+        return nullptr;
+    }
+    while (node->right != nullptr) {
+        node = node->right;
+    }
+    return node;
+}
+
+template <typename T>
+void BST<T>::insert(T const& value, Node*& node) {
+    if (node == nullptr) {
+        node = new Node(value);
+    } else if (value < node->value) {
+        insert(value, node->left);
+    } else if (value > node->value) {
+        insert(value, node->right);
+    }
+}
+
+template <typename T>
+bool BST<T>::contains(T const& value, Node* node) const {
+    if (node == nullptr) {
+        return false;
+    }
+    if (value == node->value) {
+        return true;
+    } else if (value < node->value) {
+        return contains(value, node->left);
+    } else {
+        return contains(value, node->right);
+    }
+}
+
+template <typename T>
+typename BST<T>::Node* BST<T>::remove(T const& value, Node* node) {
+    if (node == nullptr) {
+        return nullptr;
+    }
+    if (value < node->value) {
+        node->left = remove(value, node->left);
+    } else if (value > node->value) {
+        node->right = remove(value, node->right);
+    } else {
+        if (node->left == nullptr) {
+            Node* temp = node->right;
+            delete node;
+            return temp;
+        } else if (node->right == nullptr) {
+            Node* temp = node->left;
+            delete node;
+            return temp;
+        }
+
+        Node* temp = findMin(node->right);
+        node->value = temp->value;
+        node->right = remove(temp->value, node->right);
+    }
+    return node;
+}
+
+template <typename T>
+size_t BST<T>::size(Node* node) const {
+    if (node == nullptr) {
+        return 0;
+    }
+    return 1 + size(node->left) + size(node->right);
+}
+
+template <typename T>
+void BST<T>::clear(Node*& node) {
+    if (node != nullptr) {
+        clear(node->left);
+        clear(node->right);
+        delete node;
+        node = nullptr;
+    }
+}
 
 #endif
