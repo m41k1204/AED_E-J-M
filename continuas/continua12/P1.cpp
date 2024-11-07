7941659fc3594bf9ee27af9b53e617f50de6a029
m41k1204 <155772773+m41k1204@users.noreply.github.com>
2024-11-07 14:12:01 -0500

    Update P1.cpp

--- continuas/continua12/P1.cpp
+++ continuas/continua12/P1.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        std::set<int> s;
+        for (int i = 0; i < trust.size(); i++) {
+            s.insert(trust[i][0]);
+        }
+        if (s.size() != n-1) return -1;
+        int judge = 0;
+        for (int i = 1; i <= n; ++i) {
+            if (s.find(i) == s.end()) judge = i;
+        }
+        int count = 0;
+        for (int i = 0; i < trust.size(); ++i) {
+            if (trust[i][1] == judge) ++count;
+        }
+        cout << judge << endl;
+        if (count == n-1) return judge;
+        return -1;
+    }
+};
