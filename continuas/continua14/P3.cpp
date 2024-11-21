16d6a43a4d4639f68a0a020451e159ec92d9f657
jorge-qs <165625725+jorge-qs@users.noreply.github.com>
2024-11-21 14:31:17 -0500

    Update P3.cpp

--- continuas/continua14/P3.cpp
+++ continuas/continua14/P3.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int left_max = 0, right_max = 0;
+        int total_water = 0;
 
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (left_max > height[left]) {
+                    total_water += left_max - height[left];
+                } else {
+                    left_max = height[left];
+                }
+                ++left;
+            } else {
+                if (right_max > height[right]) {
+                    total_water += right_max - height[right];
+                } else {
+                    right_max = height[right];
+                }
+                --right;
+            }
+        }
+        return total_water;
+    }
+};
