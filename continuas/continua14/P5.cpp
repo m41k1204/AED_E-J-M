fb2a3b922ffb63a193fad8df0934f72ca4bf7638
jorge-qs <165625725+jorge-qs@users.noreply.github.com>
2024-11-21 14:51:42 -0500

    Update P5.cpp

--- continuas/continua14/P5.cpp
+++ continuas/continua14/P5.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    std::vector<std::string> findItinerary(std::vector<std::vector<std::string>>& tickets) {
+        std::unordered_map<std::string, std::multiset<std::string>> graph;
+        for (const auto& ticket : tickets) {
+            graph[ticket[0]].insert(ticket[1]);
+        }
 
+        std::vector<std::string> itinerary;
+        dfs("JFK", graph, itinerary);
+        std::reverse(itinerary.begin(), itinerary.end());
+        return itinerary;
+    }
+
+private:
+    void dfs(const std::string& airport,
+             std::unordered_map<std::string, std::multiset<std::string>>& graph,
+             std::vector<std::string>& itinerary) {
+        auto& destinations = graph[airport];
+        while (!destinations.empty()) {
+            auto next = destinations.begin();
+            std::string nextAirport = *next;
+            destinations.erase(next); 
+            dfs(nextAirport, graph, itinerary);
+        }
+        itinerary.push_back(airport);
+    }
+};
