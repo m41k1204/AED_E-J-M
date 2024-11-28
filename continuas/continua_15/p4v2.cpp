c04b6f67afd983383031fa981936bd07d1da0acb
jorge-qs <165625725+jorge-qs@users.noreply.github.com>
2024-11-28 15:01:51 -0500

    Create p4v2.cpp

---
+++ continuas/continua_15/p4v2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void backtrack(vector<int>& nums, vector<bool>& used, vector<int>& current, vector<vector<int>>& result) {
+        if (current.size() == nums.size()) {
+            result.push_back(current);
+            return;
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            if (used[i]) continue;
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+
+            used[i] = true;
+            current.push_back(nums[i]);
+
+            backtrack(nums, used, current, result);
+
+            used[i] = false;
+            current.pop_back();
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> current;
+        vector<bool> used(nums.size(), false);
+
+        sort(nums.begin(), nums.end());
+
+        backtrack(nums, used, current, result);
+        return result;
+    }
+};
