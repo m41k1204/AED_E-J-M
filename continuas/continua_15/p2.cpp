f831aff484d8fb86f3daeb80217f078907804364
EduardoAragon11 <166084429+EduardoAragon11@users.noreply.github.com>
2024-11-28 15:13:50 -0500

    Update p2.cpp

--- continuas/continua_15/p2.cpp
+++ continuas/continua_15/p2.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> c;
+    int tar;
+    void back(vector<int> v, int a){
+        if(a > tar) return;
+        if(a == tar) res.push_back(v);
+        int l = 0;
+        if(!v.empty()) l = v[v.size() - 1];
+        for(auto x:c){
+            if(x < l) continue;
+            v.push_back(x);
+            back(v,a+x);
+            v.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        swap(c,candidates);
+        swap(target,tar);
+        vector<int> v;
+        back(v,0);
+        return res;
+    }
+};
