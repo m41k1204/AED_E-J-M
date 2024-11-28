51403a877615933593e97ccd93331455e4ca3a08
m41k1204 <155772773+m41k1204@users.noreply.github.com>
2024-11-28 15:00:23 -0500

    Update p4.cpp

--- continuas/continua_15/p4.cpp
+++ continuas/continua_15/p4.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+    std::sort(nums.begin(), nums.end());
+    vector<vector<int>> res;
+    do
+    {
+        res.push_back(nums);
+    }
+    while (std::next_permutation(nums.begin(), nums.end()));
+    return res;
+    };
+};
